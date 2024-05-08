ee6836c0bf9e943e9775541df5991dc60e5c19ee
syk25 <syk2245@gmail.com>
2024-05-08 22:33:06 +0000

    11.9 tiny

---
+++ tiny/cgi-bin/form-adder.c
@@ -0,0 +1,43 @@
+/*
+ * adder.c - a minimal CGI program that adds two numbers together
+ */
+/* $begin adder */
+#include "csapp.h"
+
+int main(void) {
+  char *buf, *p, *q, *r; // 버퍼, 포인터
+  char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE]; // CLI 입력 인자, 컨텐츠 받기 위한 버퍼 생성
+  int n1 = 0, n2 = 0;
+  
+  /* 인자 2개 추출하기 */
+  if((buf = getenv("QUERY_STRING")) != NULL){ // 환경변수의 이름이 존재할때
+  p = strchr(buf, '&'); // 포인터에 버퍼 상의 &의 위치를 저장
+  *p = '\0'; // &를 Null로 바꿔버림 -> 문자열을 두 부분으로 구분
+  q = strchr(buf, '=');
+  r = strchr(p+1, '=');
+
+  strcpy(arg1, q+1); // 버퍼에서 Null 기준 앞의 문자열을 arg1에 저장
+  strcpy(arg2, r+1); // 버퍼에서 Null 기준 뒤의 문자열을 arg2에 저장
+  
+
+  n1 = atoi(arg1); // 문자열을 정수형으로 변환
+  n2 = atoi(arg2); // 문자열을 정수형으로 변환
+  }
+
+  /* 응답 body 만들기 */
+  sprintf(content, "QUERY_STRING=%s", buf); // 쿼리스트리의 환경변수 저장
+  sprintf(content, "Welcome to add.com: "); // content  버퍼에 문자열 저장
+  sprintf(content, "%sThe Internet addition portal. \r\n<p>", content); // 현재 내용 뒤에 문자열을 추가 후 저장
+  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2); // 덧셈 후 결과 출력
+  sprintf(content, "%sThanks for visiting!\r\n", content); // 마지막 메세지
+
+  /* HTTP 응답 생성하기 */
+  printf("Connection: close\r\n");
+  printf("Connection-length: %d\r\n", (int)strlen(content));
+  printf("Content-type: text/html\r\n\r\n");
+  printf("%s", content);
+  fflush(stdout);
+
+  exit(0);
+}
+/* $end adder */
--- tiny/tiny.c
+++ tiny/tiny.c
@@ -47,7 +47,6 @@ int main(int argc, char **argv) {
   }
 }
 
-
 // doit(): HTTP 트랜젝션 담당
 /**
  * doit()함수
@@ -73,7 +72,10 @@ void doit(int fd) {  // 식별자를 인자로 받음
 
   /* 헤더와 요청 줄 읽기 */
   Rio_readinitb(&rio, fd);
-  Rio_readlineb(&rio, buf, MAXLINE);
+  if (!Rio_readlineb(&rio, buf, MAXLINE)) {
+    return;
+  }
+
   printf("Request headers: \n");
   sscanf(buf, "%s %s %s", method, uri, version);
 
@@ -83,7 +85,7 @@ void doit(int fd) {  // 식별자를 인자로 받음
     return;
   }
 
-  read_requesthdrs(&rio); //요청 헤더 읽기
+  read_requesthdrs(&rio);  // 요청 헤더 읽기
 
   /* GET 요청으로부터 URI 파싱하기 */
   is_static = parse_uri(uri, filename,
@@ -93,7 +95,7 @@ void doit(int fd) {  // 식별자를 인자로 받음
   if (stat(filename, &sbuf) <
       0) {  // 파일이 존재하지 않는 경우, 에러 메세지 반환
     clienterror(fd, filename, "404", "Not found",
-                "Tiny couldn't find this file"); 
+                "Tiny couldn't find this file");
     return;
   }
 
@@ -119,11 +121,10 @@ void doit(int fd) {  // 식별자를 인자로 받음
   }
 }
 
-
 void read_requesthdrs(rio_t *rp) {
   char buf[MAXLINE];
 
-  Rio_readlineb(rp, buf, MAXLINE); 
+  Rio_readlineb(rp, buf, MAXLINE);
   while (strcmp(buf, "\r\n")) {
     Rio_readlineb(rp, buf, MAXLINE);
     printf("%s", buf);
@@ -172,7 +173,7 @@ void serve_static(int fd, char *filename, int filesize, char *version) {
 
   /* 클라이언트에게 응답 헤더 보내기 */
   get_filetype(filename, filetype);
-  sprintf(buf, "HTTP/%s 200 OK\r\n", version);
+  sprintf(buf, "%s 200 OK\r\n", version);
   sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
   sprintf(buf, "%sConnection: close\r\n", buf);
   sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
@@ -183,10 +184,74 @@ void serve_static(int fd, char *filename, int filesize, char *version) {
 
   /* 클라이언트에게 응답 바디 보내기 */
   srcfd = Open(filename, O_RDONLY, 0);
-  srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
-  Close(srcfd);
-  Rio_writen(fd, srcp, filesize);
-  Munmap(srcp, filesize);
+
+  /**
+   * 숙제: 11.9
+   * 1) mmap 대체하기
+   * 2) rio_writen 대체하기
+   *
+   * mmap 함수
+   * 선언: void *mmap(void *start, size_t length, int prot, int flags, int fd,
+   * off_t offset); 소개: 메모리의 특정공간에 파일을 매핑 -> 성능개선! 입력:
+   *  - start: 매핑할 메모리의 시작 주소
+   *  - length: 매핑할 메모리의 길이
+   *  - prot: 사용권한
+   *  - flags: 메모리 주소 공간 설정
+   *  - fd: 파일 식별자
+   *  - offset: 시작주소에서 떨어진 거리
+   * 출력: 없음
+   * 동작과정
+   *  1. mmap 실행 후 가상 메모리 주소에 file 주소가 매핑 됨
+   *  2. 해당 메모리에 접근할 때 OS가 파일 데이터를 복사하여 물리 메모리에
+   * 업로드
+   *  3. 메모리 read
+   *
+   * rio-writen
+   * 선언: ssize_t rio_writen(int fd, void *usrbuf, size_t n);
+   * 입력:
+   *  - fd: 파일 식별자
+   *  - usrbuf: 쓸 데이터를 가지고 있는 버퍼의 포인터
+   *  - n: 쓰려는 바이트의 수
+   * 출력:
+   * 동작과정: 네트워크 프로그래밍에서 안정적으로 데이터를 쓰기 위한 함수(요청한
+   * 바이트 수 만큼 씀)
+   *
+   * malloc
+   * 선언: void *malloc(size_t size);
+   * 입력: size: 할당 받을 바이트의 수
+   * 동작과정:
+   *  - 요청 처리
+   *  - 메모리 검색: 힙에서 요청 된 데이터 검색
+   *  - 메모리 할당: 적절한 블럭을 찾은 후 사용자에게 할당
+   *  - 메모리 분할: 블럭의 크기가 요청 크기보다 크다면 남은 공간 분할
+   *  - 메모리 초기화 안함
+   *  - 에러 처리: 요청을 처리할 수 없는 경우 NULL 포인터 반환
+   *
+   * rio_readn
+   * 선언:
+   * 입력:
+   * 출력:
+   * 동작과정:
+   *
+   * rio_writen
+   * 입력:
+   * 출력:
+   * 동작과정:
+   *  */
+
+  // 바꿔야할 코드
+  // srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+  // Close(srcfd);
+  // Rio_writen(fd, srcp, filesize);
+  // Munmap(srcp, filesize);
+
+  // 바꾼코드
+
+  srcp = (char *)Malloc(filesize);  // 파일 크기 만큼 동적할당 후 주소 반환
+  Rio_readn(srcfd, srcp, filesize);  // 파일을 읽고 srcp 에 저장
+  Close(srcfd);                      // 연결 끊기
+  Rio_writen(fd, srcp, filesize);  // 클라이언트에게 파일을 보낸다
+  Free(srcp);                      // 메모리 해제
 }
 
 void get_filetype(char *filename, char *filetype) {
@@ -198,6 +263,8 @@ void get_filetype(char *filename, char *filetype) {
     strcpy(filetype, "image/png");
   } else if (strstr(filename, ".jpg")) {
     strcpy(filetype, "image/jpeg");
+  } else if (strstr(filename, ".mp4")) {
+    strcpy(filetype, "video/mp4");  // mp4 파일 처리
   } else {
     strcpy(filetype, "text/plain");
   }
@@ -207,14 +274,14 @@ void serve_dynamic(int fd, char *filename, char *cgiargs, char *version) {
   char buf[MAXLINE], *emptylist[] = {NULL};
 
   /* HTTP 응답의 첫번째 부분 반환 */
-  sprintf(buf, "HTTP/%s 200 OK\r\n", version);
+  sprintf(buf, "%s 200 OK\r\n", version);
   Rio_writen(fd, buf, strlen(buf));
   sprintf(buf, "Server: Tiny Web Server\r\n");
   Rio_writen(fd, buf, strlen(buf));
 
   // Child
   if (Fork() == 0) {
-    /* Real Server는 모든 CGI 변수를 여기로 둠 (???)*/
+    /* Real Server는 모든 CGI 변수를 여기로 둠*/
     setenv("QUERY_STRING", cgiargs, 1);
     Dup2(fd, STDOUT_FILENO);
     Execve(filename, emptylist, environ);
@@ -223,32 +290,36 @@ void serve_dynamic(int fd, char *filename, char *cgiargs, char *version) {
 }
 /**
  * clienterror()
- * 
-  */
-
+ *
+ */
 
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
-                 char *longmsg) {  
-  char buf[MAXLINE]; // HTTP 응답헤더 저장할 버퍼
-  char body[MAXBUF]; // HTTP 응답본문 저장할 버퍼
+                 char *longmsg) {
+  char buf[MAXLINE];  // HTTP 응답헤더 저장할 버퍼
+  char body[MAXBUF];  // HTTP 응답본문 저장할 버퍼
 
   /* HTTP 응답 body 만들기 */
-  sprintf(body, "<html><title>Tiny Error</title>"); // html 파일 제목
+  sprintf(body, "<html><title>Tiny Error</title>");  // html 파일 제목
   sprintf(body,
           "%s<body bgcolor="
           "ffffff"
           ">\r\n",
-          body); 
-  sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg); // 버퍼에 형식 문자열을 대체해서 저장
-  sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause); // 상동
-  sprintf(body, "%s<hr><em>The Tiny Web Server</em>\r\n", body); // 상동
+          body);
+  sprintf(body, "%s%s: %s\r\n", body, errnum,
+          shortmsg);  // 버퍼에 형식 문자열을 대체해서 저장
+  sprintf(body, "%s<p>%s: %s</p>\r\n", body, longmsg, cause);          // 상동
+  sprintf(body, "%s<hr><em>The Tiny Web Server</em></hr>\r\n", body);  // 상동
 
   /* HTTP 응답 헤더 만들기 */
-  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg); // 상동: buf에  HTTP/1.0 에러코드 에러메세지 쓰기
-  Rio_writen(fd, buf, strlen(buf)); // 버퍼에서 strlen(buf)의 길이만큼 꺼내서 식별자가 가리키는 파일(fd)에 씀(참고로 버퍼에는 어떤 영향도 가지 않음)
-  sprintf(buf, "Content-type: text/html\r\n"); // 버퍼에 데이터 씀
-  Rio_writen(fd, buf, strlen(buf)); // fd에 버퍼의 길이만큼 씀
-  sprintf(buf, "content-length: %d\r\n\r\n", (int)strlen(body)); // 상동
-  Rio_writen(fd, buf, strlen(buf)); // 버퍼 메세지 쓰기
-  Rio_writen(fd, body, strlen(body)); // HTTP 바디 쓰기
+  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum,
+          shortmsg);  // 상동: buf에  HTTP/1.0 에러코드 에러메세지 쓰기
+  Rio_writen(
+      fd, buf,
+      strlen(buf));  // 버퍼에서 strlen(buf)의 길이만큼 꺼내서 식별자가 가리키는
+                     // 파일(fd)에 씀(참고로 버퍼에는 어떤 영향도 가지 않음)
+  sprintf(buf, "Content-type: text/html\r\n");  // 버퍼에 데이터 씀
+  Rio_writen(fd, buf, strlen(buf));  // fd에 버퍼의 길이만큼 씀
+  sprintf(buf, "content-length: %d\r\n\r\n", (int)strlen(body));  // 상동
+  Rio_writen(fd, buf, strlen(buf));    // 버퍼 메세지 쓰기
+  Rio_writen(fd, body, strlen(body));  // HTTP 바디 쓰기
 }
